5441c12ca779aef2565e402e3bd7139a5b23c883
Victor Hugo Silva <102427408+victorhugosilvablancas@users.noreply.github.com>
2023-05-25 18:26:11 -0600

    Exportardata.java

--- Exportardata.java
+++ Exportardata.java
@@ -21,53 +21,10 @@ public class Exportardata {
         
     }
     
+    /**
+    export JTable to LaText format
+    **/
     public static void TablaMDPI(String atitulo,JTable atabla) {
-        int r=atabla.getRowCount();
-        if (r>0) {
-            //columnas
-            int n=atabla.getColumnCount();
-            
-            String cadena="\\begin{table}[H]\n"
-                    + "\\caption{"+atitulo+"\\label{tab1}\\textsuperscript{1}} \n";
-            if (n>4) cadena+="\\begin{adjustwidth}{-\\extralength}{0cm}\n";
-            cadena+="\\newcolumntype{C}{>{\\centering\\arraybackslash}X} \n";
-            if (n>4) cadena+="\\begin{tabularx}{\\fulllength}{";
-            else cadena+="\\begin{tabularx}{\\textwidth}{";
-            for (int j=0;j<n;j++) {
-                cadena+="C";
-            }
-            cadena+="} \n"
-                    + "\\toprule \n";
-            n=atabla.getColumnCount();
-            int max=n-1;
-            String celda="";
-            for (int j=0;j<n;j++) {
-                celda=atabla.getColumnName(j);
-                cadena+="\\textbf{"+celda+"}";
-                if (j<max) cadena+="\t& ";
-            }
-            cadena+="\\\\\n";
-            cadena+="\\bottomrule\n";
-            for (int i=0;i<r;i++) {
-                cadena+="\\mirule\n";
-                for (int j=0;j<n;j++) {
-                    celda=(String)atabla.getValueAt(i,j);
-                    cadena+=celda;
-                    if (j<max) cadena+="\t& ";
-                }
-                cadena+="\\\\\n";
-            }
-            cadena+="\\bottomrule\n"
-                    + "\\end{tabularx}\n";
-            if (n>4) cadena+="\\end{adjustwidth}\n";
-            cadena+="\\noindent{\\footnotesize{\\textsuperscript{1} Elaboración propia.}}\n"
-                    + "\\end{table}\n";
-            
-            StringSelection stringSelection = new StringSelection(cadena);
-            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-            clipboard.setContents(stringSelection, null);
-            
-            Exportardata.Mensaje="Los datos ya están en el portapapeles.";
-        }
+        //code
     }
 }
